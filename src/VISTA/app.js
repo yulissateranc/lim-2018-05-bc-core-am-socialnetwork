7ebb5f3e53a907a3b542d0e9d9b2d7c21adb4594
Jossielinn <josi_lady@hotmail.com>
2018-07-08 03:55:25 -0500

    Conexion de login con Facebook y Google

---
+++ src/VISTA/app.js
@@ -0,0 +1,10 @@
+//Initialize Firebase
+let config = {
+    apiKey: "AIzaSyD69NK6i5GCpkrP6cd8sRjaX-0ujSQuKvs",
+    authDomain: "socialnetwork-6674c.firebaseapp.com",
+    databaseURL: "https://socialnetwork-6674c.firebaseio.com",
+    projectId: "socialnetwork-6674c",
+    storageBucket: "",
+    messagingSenderId: "648022811826"
+  };
+  firebase.initializeApp(config);
