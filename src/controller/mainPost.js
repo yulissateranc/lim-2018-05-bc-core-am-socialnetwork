aedc6ccc6181646901131b235fafd47db0372c38
Jossielinn <josi_lady@hotmail.com>
2018-07-23 00:16:20 -0500

    CRUD - Fx publicar,actualizar, visualizar y modificar en tiempo real con conexion a firebase

--- src/controller/mainPost.js
+++ src/controller/mainPost.js
@@ -1,5 +1,3 @@
-
-
 const buttonPublicPost = document.getElementById('btn-public-post');
 
 
@@ -7,22 +5,151 @@ buttonPublicPost.addEventListener('click', (e) => {
 	let descriptionPost = document.getElementById('txt-description-post');
 	let likesCount = document.getElementById('input-likes-count');
 	e.preventDefault();
-	const posts = document.getElementById('posts');
-	newPost = createPost(descriptionPost, likesCount);
-	posts.innerHTML += `
+     if (descriptionPost.value !== '') {
+      createPost(descriptionPost,likesCount);
+      descriptionPost.value ="";
+    }
+    else {
+        alert('Escriba su opinion');
+        descriptionPost.placelholder = "Escribe un mensaje";
+    }
+	/*posts.innerHTML += `
 	<form class="comentary">
 	<!--<button type="button" id="seleccion" class="icon-ellipsis-vert"></button>-->
 	<textarea  id="${newPost}" name="textarea" rows="4" cols="50">${descriptionPost.value}</textarea>
-	<input type="number" id="textValuefixed" readonly/>
+	<input type="number" class="textValuefixed" readonly/>
 	<button type="button" class="icon-ok"></button>
 	<div class="section-seleccion">
-			<button type="button">Editar</button>
-			<button type="button" id="cerrar-sesion">Eliminar</button>
+			<button type="button" data-message-edit= ${newPost}>Editar</button>
+			<button type="button" id="cerrar-sesion" data-message-delete="${newPost}">Eliminar</button>
 	</div>
-	</form>`
-
-
+	</form>`*/
 });
+
+const borrarDatosFirebase = () => {
+    let refPost = (firebase.database().ref().child('POST'));
+    let keyDataDelete = event.target.getAttribute("data-message-delete");
+    console.log(keyDataDelete);
+    let refMesaggeDelete = refPost.child(keyDataDelete);
+    if (confirm("Esta seguro de borrar el mensaje ?")) {
+        refMesaggeDelete.remove();
+    }
+}
+
+
+const editaDatosFirebase = () => {
+    alert('editar');
+    const posts = document.getElementById('posts');
+    let keyDataEdit = event.target.getAttribute("data-message-edit");
+    console.log(keyDataEdit);
+    refPost.on("value", function (snap) {
+        let datos = snap.val();
+        console.log(datos);
+        posts.innerHTML = "";
+
+        for (let key in datos) {
+            console.log(key);
+            if (key == keyDataEdit) {
+                posts.innerHTML +=
+                    `<form class="comentary">
+            <p class="users" >${datos[key].autor}</p>
+            <textarea name="postMessage" rows="4" cols="50" class="mensaje" id="text-save">  ${datos[key].description}</textarea>
+            <input type="number" class="textValuefixed" readonly /*value="${datos[key].likesCount}*/"/>
+            <button type="button" class="icon-ok"></button>
+            <button type="button" id="btn-edit" class="save" data-message-save= ${key}>Update</button>
+            <button type="button" class="borrar" data-message-delete=${key}  onclick=mostrarPost()>Cancelar</button>
+
+            </div>
+
+        </form>
+            `
+            } else {
+       posts.innerHTML += `<form class="comentary">
+        <p class="users" >${datos[key].autor}</p>
+            <textarea name="postMessage" rows="4" cols="50" class="mensaje" readonly> ${datos[key].description} </textarea>
+            <input type="number" class="textValuefixed" /*value="${datos[key].likesCount}*/" readonly/>
+            <button type="button" class="icon-ok"></button>
+            <button disabled type="button" id="btn-edit" class="editar">Editar</button>
+            <button disabled type="button" class="borrar">Eliminar</button>
+        </form>
+ `
+            };
+        }
+        if (posts != "") {
+            const elementGuardar = document.getElementsByClassName("save");
+            for (let i = 0; i < elementGuardar.length; i++) {
+        elementGuardar[i].addEventListener('click', updateU, false);
+            }
+
+        }
+
+
+    })
+
+}
+
+const updateU = () => {
+    let keyDataSave = event.target.getAttribute("data-message-save");
+    let refMesaggesave = refPost.child(keyDataSave);
+    console.log(refMesaggesave);
+    let newPost = document.getElementById("text-save").value;
+
+    refPost.on("value", function (snap) {
+        let data = snap.val();
+        for (key in data) {
+            if (key == keyDataSave) {
+                console.log(keyDataSave);
+                if (newPost === '') {
+                    alert("Incompleto");
+                }
+                else {
+                    data[key].mensaje = newPost;
+
+                    let nuevoPost = {
+                        description: newPost,
+
+                    }
+
+                    console.log(nuevoPost);
+                    var updatesPost = {};
+                    updatesPost = nuevoPost;
+             refMesaggesave.update(updatesPost)
+                    console.log(newPost);
+                    mostrarPost();
+                }
+            }
+        }
+    })
+
+
+}
+
+
+
+
+mostrarPost();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
--- src/model/bd.js
+++ src/model/bd.js
@@ -80,18 +80,6 @@ const getDataCurrentUser = () => {
   }
 }
 
-//*****************************************Create / Edite/ Remove  de los Post*****************************************************************+/
-createPost = (descriptionPost, likesCount) => {
-  alert('soy la funcion que creará el Post');
-  let refPost = (firebase.database().ref().child('POST'));
-  refPost.push({
-    postId: firebase.auth().currentUser.uid,
-    autor: firebase.auth().currentUser.displayName,
-    description: descriptionPost.value,
-    likesCount: likesCount.value
-  });
-}
-
 createUser=()=> {
   alert('se va a crear una referencia para el users');
   let refUser = (firebase.database().ref().child('USERSITOS'));
@@ -101,5 +89,58 @@ createUser=()=> {
     userEmail: firebase.auth().currentUser.email,
     userPhotoUrl: firebase.auth().currentUser.photoURL
   });
+}
+
+//*****************************************Create / Edite/ Remove  de los Post*****************************************************************+/
+const refPost = (firebase.database().ref().child('POST'));
+
+
+createPost = (descriptionPost, likesCount) => {
+  alert('soy la funcion que creará el Post');
+  let refPost = (firebase.database().ref().child('POST'));
+  refPost.push({
+    postId: firebase.auth().currentUser.uid,
+    autor: firebase.auth().currentUser.displayName,
+    description: descriptionPost.value,
+    /*likesCount: likesCount.value*/
+  });
+}
+
+mostrarPost = () => {
+   let refPost = (firebase.database().ref().child('POST'));
+    refPost.on("value", function(snap) {
+        let datos = snap.val();
+       // console.log(datos);
+        const viewPost = document.getElementById('posts');
+        let elementsView = "";
+        for (let key in datos) {
+
+           elementsView += `
+                
+        <form class="comentary">
+            <p class="users" >${datos[key].autor}</p>
+            <textarea name="postMessage" rows="4" cols="50" readonly class="mensaje">  ${datos[key].description}</textarea>
+            <input type="number" class="textValuefixed" readonly /*value="${datos[key].likesCount}"*//>
+            <button type="button" class="icon-ok"></button>
+            <button type="button" id="btn-edit" class="editar" data-message-edit= ${key}>Editar</button>
+            <button type="button" class="borrar" data-message-delete=${key}>Eliminar</button>
+
+            </div>
+
+        </form>
+            `
+        }
+        viewPost.innerHTML = elementsView;
+        if (elementsView!= "") {
+            const elementDelete = document.getElementsByClassName("borrar");
+            const elementEdit = document.getElementsByClassName("editar");
+            for (let i = 0; i < elementDelete.length; i++) {
+                elementDelete[i].addEventListener('click', borrarDatosFirebase, false);
+                elementEdit[i].addEventListener('click', editaDatosFirebase, false);
+
+            }
+        }
+        
+    });
 }
 
